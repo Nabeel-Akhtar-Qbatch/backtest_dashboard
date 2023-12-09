fe23c42090d74ab2650df1f8355cbca7f2ab2075
Nabeel-Akhtar-Qbatch <nabeel.akhtar@qbatch.com>
2023-12-10 01:18:58 +0500

    update optimal trade

--- src/app/optimal-trade/page.js
+++ src/app/optimal-trade/page.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Table,
   TableBody,
@@ -69,39 +70,97 @@ export default function Home() {
       },
     },
   ];
-  const columns = ["1.5", "2", "3", "5"];
+  const columns = ["1.5", "2", "3", "5", "6"]; // Use the column labels as they appear in your data keys
+  const subColumns = [
+    { label: "Gross Profit", key: "grossProfit" },
+    { label: "Gross Loss", key: "grossLoss" },
+    { label: "Net Profit", key: "netProfit" },
+    { label: "Maximum Consecutive Loss", key: "maximumConsecutiveLoses" }, // Ensure this key matches your data
+  ];
+
+  const styles = {
+    header: {
+      backgroundColor: "#0070f3",
+      color: "white",
+      padding: "15px",
+      fontSize: "18px",
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    subHeader: {
+      backgroundColor: "#e6e6e6",
+      padding: "15px",
+      fontSize: "16px",
+      fontWeight: "normal",
+      textAlign: "center",
+    },
+    row: {
+      backgroundColor: "white",
+      textAlign: "center",
+      padding: "12px 15px",
+      fontSize: "14px",
+    },
+    altRow: {
+      backgroundColor: "#f2f2f2",
+      textAlign: "center",
+      padding: "12px 15px",
+      fontSize: "14px",
+    },
+  };
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead></TableHead>
-          {columns.map((column) => (
-            <TableHead className="text-center" colSpan={2}>
-              {column}R
-            </TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map(({ name, data }) => (
+    <div style={{ overflowX: "auto" }}>
+      <Table>
+        <TableHeader>
           <TableRow>
-            <TableCell>{name}</TableCell>
+            <TableHead style={styles.header} colSpan="2">
+              Strategy
+            </TableHead>
             {columns.map((column) => (
-              <>
-                <TableCell className="text-center">
-                  <div>{data[column]?.grossProfit}</div>
-                  <div>{data[column]?.grossProfit}</div>
-                </TableCell>
-                <TableCell className="text-center">
-                  <div>{data[column]?.netProfit}</div>
-                  <div>{data[column]?.maximumConsecutiveLoses}</div>
-                </TableCell>
-              </>
+              <TableHead
+                key={column + "R"}
+                style={styles.header}
+                colSpan={subColumns.length}
+              >
+                {column + "R"}
+              </TableHead>
             ))}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+          <TableRow>
+            <TableCell style={styles.subHeader}></TableCell>{" "}
+            {/* Empty cell for alignment */}
+            {columns.flatMap((column) =>
+              subColumns.map((subColumn) => (
+                <TableCell
+                  key={`${column}-${subColumn.key}`}
+                  style={styles.subHeader}
+                >
+                  {subColumn.label}
+                </TableCell>
+              ))
+            )}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((entry, index) => (
+            <TableRow key={entry.name}>
+              <TableCell style={index % 2 === 0 ? styles.row : styles.altRow}>
+                {entry.name}
+              </TableCell>
+              {columns.flatMap((column, colIdx) =>
+                subColumns.map((subColumn, subIdx) => (
+                  <TableCell
+                    key={`${entry.name}-${column}-${subColumn.key}`}
+                    style={index % 2 === 0 ? styles.row : styles.altRow}
+                  >
+                    {entry.data[column]?.[subColumn.key]}
+                  </TableCell>
+                ))
+              )}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
